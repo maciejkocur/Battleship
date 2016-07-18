f8079afde4f1a9603328e52fa9e1bed909807c53
kuba <czyzu0507@gmail.com>
2016-07-18 14:24:46 +0200

    Turn manager TurnProvider is used to manage which player is current  and next.

---
+++ Core/src/main/java/model/turnProvider/TurnProvider.java
@@ -0,0 +1,34 @@
+package model.turnProvider;
+
+import model.player.Player;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+public class TurnProvider {
+
+    private Queue<Player> turnQueue;
+
+    private TurnProvider(Queue<Player> turnQueue) {
+        this.turnQueue = turnQueue;
+    }
+
+    public static TurnProvider getTurnProvider(Player player1, Player player2) {
+        Queue<Player> turnQueue = new ArrayDeque<>();
+        turnQueue.addAll(Arrays.asList(player1, player2));
+        return new TurnProvider(turnQueue);
+    }
+
+
+    public Player getCurrentPlayer() {
+        return turnQueue.peek();
+    }
+
+    public Player getNextPlayer() {
+        Player currentPlayer = turnQueue.poll();
+        turnQueue.add(currentPlayer);
+        return turnQueue.peek();
+    }
+
+}
