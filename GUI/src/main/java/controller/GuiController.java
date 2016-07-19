66526231480fa1b186d7c260cb8a9c1c442a3557
Bartlomiej <bartomiej.lazarski@gmail.com>
2016-07-19 16:56:53 +0200

    Need to change the placeholder image for already shot places, as for now the place is disabled

--- GUI/src/main/java/controller/GuiController.java
+++ GUI/src/main/java/controller/GuiController.java
@@ -5,10 +5,16 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,22 +23,22 @@ import java.util.ResourceBundle;
  */
 public class GuiController {
 
-    @FXML private Button button ;
 
 
     @FXML
     private void fireAway(ActionEvent event){
         Node source = (Node)event.getSource();
+
+        source.setOpacity(1.0);
+
+
         System.out.println("Hello " + "This is the row : "+GridPane.getRowIndex(source)+"\nAnd column: "+ GridPane.getColumnIndex(source));
+
+
+        source.setDisable(true);
         }
 
 
 
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//
-//        @Override
-//        public void handle(ActionEvent event) {
-//            lbl.setText("Hello, World.");
-//        }
-//    });
+
 }
