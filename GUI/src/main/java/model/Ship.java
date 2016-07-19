2b0a20961652ebb140121e802854d38b4ed33e49
Bartlomiej <bartomiej.lazarski@gmail.com>
2016-07-19 13:53:12 +0200

    Initialized the enemy board, so that clicking on each cell will result in action(to be implemented). It should give us back the coordinates so the GameEngine can process them

---
+++ GUI/src/main/java/model/Ship.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by bartlomiej on 19.07.16.
+ */
+public class Ship {
+}
---
+++ GUI/src/test/java/ShipTest.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by bartlomiej on 19.07.16.
+ */
+public class ShipTest {
+}
