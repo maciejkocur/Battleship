b7010a708d3c43583d8ee7281e269d3e4cfcff5c
Bartlomiej <Bartlomiej_lazarski@epam.com>
2016-07-19 10:16:12 +0200

    Initialized most of the static parts of the gui, it is time to add the events as well as the ships, buttons and the game flow itself

--- GUI/src/main/java/Gui.java
+++ GUI/src/main/java/Gui.java
@@ -1,5 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by DELL on 2016-07-18.
  */
-public class Gui {
+public class Gui extends Application{
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("gui.fxml"));
+
+        Scene scene = new Scene(root, 1200, 800);
+
+        primaryStage.setTitle("Battleship");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
