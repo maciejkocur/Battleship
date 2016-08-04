37ab7beb5dfd3d05f214e34f67d65119e63c862e
Bartlomiej <bartomiej.lazarski@gmail.com>
2016-08-04 10:46:10 +0200

    It seems that the MainController is not needed at this point, I am starting the gui, and we are booting up the tomcat with Spring boot. This will do only for this moment, as we actually need to implement REST communication and configuration but it seems that I will be able to run Spring + JavaFX and that solves one problem for now. The next step will be to create the configuration to put the coordinates on the server as JSON

--- GUI/src/main/java/view/Gui.java
+++ GUI/src/main/java/view/Gui.java
@@ -1,36 +1,47 @@
 package view;
 
+import controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.Player;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Created by DELL on 2016-07-18.
  */
+@SpringBootApplication
 public class Gui extends Application{
 
+    private static String [] args;
     private Player player;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         player = Player.getPlayer();
-
-        try{Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("gui.fxml"));
+        ApplicationContext context = SpringApplication.run(Gui.class,args);
+       // MainController mainController = context.getBean(MainController.class);
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("gui.fxml"));
         Scene scene = new Scene(root, 1200, 800);
+       // Scene scene = new Scene((Parent) mainController.getRoot());
+
+
         primaryStage.setResizable(false);
         primaryStage.setTitle("Battleship");
         primaryStage.setScene(scene);
         primaryStage.show();
         }
 
-        catch(Exception e){}
-    }
+
+
 
     public static void main(String[] args){
+        Gui.args=args;
         launch(args);
     }
 }
